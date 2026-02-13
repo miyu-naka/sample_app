cd5ef66514fe0763d60b5c7c98e5f68481d22b27
Miyu Nakajima <miyu1357star@icloud.com>
2026-02-13 10:42:01 +0900

    Adminユーザー機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-  user = User.find_by(email: params[:session][:email].downcase)
-  if user&.authenticate(params[:session][:password])
-    forwarding_url = session[:forwarding_url]
-    reset_session
-    log_in user
-    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    redirect_to forwarding_url || user
-  else
-    flash.now[:danger] = 'Invalid email/password combination'
-    render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
+      reset_session
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to forwarding_url || user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
   end
-end
 
   def destroy
     log_out if logged_in?
