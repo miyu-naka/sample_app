3e23e76177d132fe73ebbe0f6da6b5148155dae1
Miyu Nakajima <miyu1357star@icloud.com>
2026-02-24 10:23:56 +0900

    followテストのデータ作成

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2026_02_18_035218) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
